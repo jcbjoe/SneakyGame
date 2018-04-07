9dd9ec1586cf5bc7251fad1320597329a395e06e
Joe Garlick <bradleybrad1@hotmail.co.uk>
2018-04-08 00:48:35 +0100

    level Manager fixed

--- MazeSKEL/Level.cpp
+++ MazeSKEL/Level.cpp
@@ -10,8 +10,9 @@ Level::Level(string name)
 }
 
 void Level::setLevelMap(int level[10][10]) {
-	for (int i = 10; i < 10; i++) {
-		for (int j = 10; j < 10; j++) {
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
 			levelMap[i][j] = level[i][j];
 		}
 	}
