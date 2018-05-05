c8fcd823ad4f81c79573705f5168b59af88f315e
Unknown <alex.fullarton@live.com>
2018-05-05 14:31:04 +0100

    Added test lighting to level 1

--- MazeSKEL/Level1.cpp
+++ MazeSKEL/Level1.cpp
@@ -27,27 +27,13 @@ Level1::Level1(string name): Level(name) {
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 	};
 
+	//Lighting tests
+
+	FX::SetupSpotLight(1, true, { 9, 1, 8 }, {0, -1, 0}, Vector3(0.1f, 0.3f, 0.1f), Vector3(0.35f, 0.8f, 0.35f), Vector3(0.15f, 0.15f, 0.15f), 3.0f, 0.5f, D2R(15), D2R(70));
+
 
 	setLevelMap(level);
 
-	//std::vector<Vector3> waypointLocations = 
-	//{
-	//	Vector3(8, 0.4, 6),
-	//	Vector3(7, 0.4, 6),
-	//	Vector3(7, 0.4, 1),
-	//	Vector3(4, 0.4, 1),
-	//	Vector3(4, 0.4, 7),
-	//	Vector3(5, 0.4, 7),
-	//	Vector3(5, 0.4, 8),
-	//	Vector3(8, 0.4, 8 )
-	//};
-	//
-	//addWaypointLocation(Vector3(8, 0.4, 6));
-	//
-	//int count(1);
-	//for (Vector3 vec : waypointLocations) {
-	//	addWaypointLocation(vec);
-	//}
 }
 
 void Level1::Release() {
