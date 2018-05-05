36f7629cd48e077d733877c436acd761fb4282b4
Unknown <alex.fullarton@live.com>
2018-05-05 15:24:37 +0100

    Texture lighting changes for loot/ walls & floor/ceiling

--- MazeSKEL/Loot.cpp
+++ MazeSKEL/Loot.cpp
@@ -9,7 +9,7 @@ Loot::Loot(string name, Vector3 position, Vector3 rotation, Vector3 scale)
 	GameObject::setInitialPos();
 
 	MaterialExt mat = GetModel().GetMesh().GetSubMesh(0).material;
-	mat.gfxData.Set(Vector4(0.5, 0.5, 0.5, 1), Vector4(1, 1, 1, 0), Vector4(1, 1, 1, 1));
+	mat.gfxData.Set(Vector4(0.8, 0.8, 0.8, 1), Vector4(0.4, 0.4, 0.4, 0), Vector4(1, 1, 1, 1));
 	mat.pTextureRV = FX::GetMyFX()->mCache.LoadTexture("CoinBase.dds", true, gd3dDevice);
 	mat.texture = "CoinBase.dds";
 	GetModel().SetOverrideMat(&mat);
--- MazeSKEL/ReturnBox.cpp
+++ MazeSKEL/ReturnBox.cpp
@@ -11,7 +11,7 @@ ReturnBox::ReturnBox(string name, Vector3 position, Vector3 rotation, Vector3 sc
 	GameObject::setInitialPos();
 
 	MaterialExt mat = GetModel().GetMesh().GetSubMesh(0).material;
-	mat.gfxData.Set(Vector4(0.5, 0.5, 0.5, 1), Vector4(1, 1, 1, 1), Vector4(0.1, 0.1, 0.1, 1));
+	mat.gfxData.Set(Vector4(0.5, 0.5, 0.5, 0), Vector4(0.4, 0.4, 0.4, 0), Vector4(0.1, 0.1, 0.1, 1));
 	mat.pTextureRV = FX::GetMyFX()->mCache.LoadTexture("chest.dds", true, gd3dDevice);
 	mat.texture = "chest.dds";
 
