beefb798da4460294ebe387d98742cb4d4fce0a5
David Harrison <dave.k.harrison@gmail.com>
2018-05-10 11:11:47 +0100

    Commented Keys and Removed Unnecessay Code

--- MazeSKEL/BlueKey.cpp
+++ MazeSKEL/BlueKey.cpp
@@ -7,16 +7,11 @@ BlueKey::BlueKey(string name, Vector3 position, Vector3 rotation, Vector3 scale)
 	GetModel().Initialise(*GetMeshManager()->GetMesh("Key"));
 
 	GameObject::setInitialPos();
-
-	MaterialExt mat = GetModel().GetMesh().GetSubMesh(0).material;
-	mat.gfxData.Set(Vector4(0.5, 0.5, 0.5, 1), Vector4(0, 0, 1, 0), Vector4(1, 1, 1, 1));
-	mat.pTextureRV = FX::GetMyFX()->mCache.LoadTexture("Key.dds", true, gd3dDevice);
-	mat.texture = "Key.dds";
-	GetModel().SetOverrideMat(&mat);
 }
 
 void BlueKey::Update(float dTime)
 {
+	//Sets up the Key Texture and Makes it Scroll
 	MaterialExt mat = GetModel().GetMesh().GetSubMesh(0).material;
 	mat.gfxData.Set(Vector4(0.5, 0.5, 0.5, 1), Vector4(0, 0, 1, 0), Vector4(1, 1, 1, 1));
 	mat.pTextureRV = FX::GetMyFX()->mCache.LoadTexture("Key.dds", true, gd3dDevice);
@@ -25,8 +20,8 @@ void BlueKey::Update(float dTime)
 	GetModel().SetOverrideMat(&mat);
 	textMov += dTime * 0.5;
 
-	//GetRotation() = GetRotation() + Quaternion::CreateFromAxisAngle(Vector3(0, 1, 0), GetRotationAngle() += dTime * 0.5);
 	if (getMove()) {
+		//Makes key fly up on collect
 		incrementRotation(dTime * 8, 0, 0);
 		collectMov += dTime;
 		GetModel().GetPosition() = GetPosition() + Vector3(0, collectMov, 0);
@@ -34,6 +29,7 @@ void BlueKey::Update(float dTime)
 			GetGameObjectManager()->deleteGameObjectByIndex(getIndex());
 	}
 	else {
+		//makes key rotate and bob on idle
 		incrementRotation(dTime * 2, 0, 0);
 		idleMov += dTime;
 		GetModel().GetPosition() = GetPosition() + Vector3(0, sin(idleMov) / 10, 0);
